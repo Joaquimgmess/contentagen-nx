5cf37ef937ef9178d994f89c60077f61e9027524
Joaquimgmess <joaquimgmess21@gmail.com>
2025-09-17 23:00:22 -0300

    Add localization for agent, content and ideas lists

--- packages/localization/src/locales/en-US/index.ts
+++ packages/localization/src/locales/en-US/index.ts
@@ -1,8 +1,14 @@
 import homePageEnUs from "./pages/home.json";
+import agentListEnUs from "./pages/agent-list.json";
+import contentListEnUs from "./pages/content-list.json";
+import ideasListEnUs from "./pages/ideas-list.json";
 
 const resources = {
    translation: {
       "pages.home": homePageEnUs,
+      "pages.agent-list": agentListEnUs,
+      "pages.content-list": contentListEnUs,
+      "pages.ideas-list": ideasListEnUs,
    },
 };
 
--- packages/localization/src/locales/pt-BR/index.ts
+++ packages/localization/src/locales/pt-BR/index.ts
@@ -1,8 +1,14 @@
 import homePagePtBr from "./pages/home.json";
+import agentListPtBr from "./pages/agent-list.json";
+import contentListPtBr from "./pages/content-list.json";
+import ideasListPtBr from "./pages/ideas-list.json";
 
 const resources = {
    translation: {
       "pages.home": homePagePtBr,
+      "pages.agent-list": agentListPtBr,
+      "pages.content-list": contentListPtBr,
+      "pages.ideas-list": ideasListPtBr,
    },
 };
 
